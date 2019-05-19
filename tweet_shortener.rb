edc2b1b9b6c867bdebc60e8da20290450cdae517
Elijah Florence <efloren@siue.edu>
2019-05-19 19:42:20 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def dictionary(string)
+word = string
+if word == "hello"
+"hi"
+elsif word == "to" || word == "two" || word == "too"
+"2"
+elsif word == "for" || word == "four"
+"4"
+elsif word == "be"
+"b"
+elsif word == "you"
+"u"
+elsif word == "at"
+"@"
+elsif word == "and"
+"&"
+end
+end
+
+def word_substituter(tweet)
+  
